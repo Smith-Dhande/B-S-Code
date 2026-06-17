a32de3994579c8eb9fa18af9aad6bc1f182a2331
Smith-Dhande <smithdhande@gmail.com>
2026-06-17 19:10:52 +0530

    feat(editor): integrate Monaco and open file functionality

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 import { selectFolder } from "./filesystem/openFolder.js";
+import { createEditor } from "./editor/monaco.js";
+
+createEditor();
 
 let openFolderBtn = document.getElementById("open-folder-button");
 
--- js/editor/monaco.js
+++ js/editor/monaco.js
@@ -1,1 +1,31 @@
+let editor;
 
+function createEditor() {
+
+    require.config({
+        paths: {
+            vs: "./vendor/monaco/vs"
+        }
+    });
+
+    require(["vs/editor/editor.main"], function () {
+
+        editor = monaco.editor.create(
+            document.getElementById("editor"),
+            {
+                value: "",
+                language: "javascript",
+                theme: "vs-dark",
+                automaticLayout: true
+            }
+        );
+
+    });
+
+}
+
+function getEditor() {
+    return editor;
+}
+
+export { createEditor, getEditor };
